728ef45ffd0c68ff6707095ea546637040892af5
Choisooyoung98 <ekfms15815@gmail.com>
2022-10-17 10:53:56 +0900

    세팅

---
+++ src/test/java/com/ll/exam/mutbooks/MutbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.mutbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
