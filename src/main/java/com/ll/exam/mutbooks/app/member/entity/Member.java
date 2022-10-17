e6ab3bf27b2017589a5d9307685d60e5f25c6098
Choisooyoung98 <ekfms15815@gmail.com>
2022-10-17 13:29:27 +0900

    [Add] Member, Security

---
+++ src/main/java/com/ll/exam/mutbooks/app/member/entity/Member.java
@@ -0,0 +1,32 @@
+package com.ll.exam.mutbooks.app.member.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ll.exam.mutbooks.app.base.entity.BaseEntity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
+    @Column(unique = true)
+    private String username;
+    @JsonIgnore
+    private String password;
+    private String email;
+    private boolean emailVerified;
+
+    public String getName() {
+        return username;
+    }
+
+    public Member(long id) {
+        super(id);
+    }
+}
