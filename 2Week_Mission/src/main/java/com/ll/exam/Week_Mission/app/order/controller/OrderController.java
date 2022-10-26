b17038806e012cb69e0dee9068fca0bb8d93fce8
Choisooyoung98 <ekfms15815@gmail.com>
2022-10-26 10:42:57 +0900

    [Feat] 예치금과 Pg결제 같이 진행

--- 2Week_Mission/src/main/java/com/ll/exam/Week_Mission/app/order/controller/OrderController.java
+++ 2Week_Mission/src/main/java/com/ll/exam/Week_Mission/app/order/controller/OrderController.java
@@ -8,6 +8,7 @@ import com.ll.exam.Week_Mission.app.order.entity.Order;
 import com.ll.exam.Week_Mission.app.order.exception.ActorCanNotPayOrderException;
 import com.ll.exam.Week_Mission.app.order.exception.ActorCanNotSeeOrderException;
 import com.ll.exam.Week_Mission.app.order.exception.OrderIdNotMatchedException;
+import com.ll.exam.Week_Mission.app.order.exception.OrderNotEnoughRestCashException;
 import com.ll.exam.Week_Mission.app.order.service.OrderService;
 import com.ll.exam.Week_Mission.app.security.dto.MemberContext;
 import com.ll.exam.Week_Mission.util.Ut;
@@ -95,7 +96,8 @@ public class OrderController {
             @RequestParam String paymentKey,
             @RequestParam String orderId,
             @RequestParam Long amount,
-            Model model
+            Model model,
+            @AuthenticationPrincipal MemberContext memberContext
     ) throws Exception {
 
         Order order = orderService.findForPrintById(id).get();
@@ -114,7 +116,15 @@ public class OrderController {
 
         Map<String, String> payloadMap = new HashMap<>();
         payloadMap.put("orderId", orderId);
-        payloadMap.put("amount", String.valueOf(order.calculatePayPrice()));
+        payloadMap.put("amount", String.valueOf(amount));
+
+        Member actor = memberContext.getMember();
+        long restCash = memberService.getRestCash(actor);
+        long payPriceRestCash = order.calculatePayPrice() - amount;
+
+        if (payPriceRestCash > restCash) {
+            throw new OrderNotEnoughRestCashException();
+        }
 
         HttpEntity<String> request = new HttpEntity<>(objectMapper.writeValueAsString(payloadMap), headers);
 
@@ -122,7 +132,7 @@ public class OrderController {
                 "https://api.tosspayments.com/v1/payments/" + paymentKey, request, JsonNode.class);
 
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            orderService.payByTossPayments(order);
+            orderService.payByTossPayments(order, payPriceRestCash);
 
             return "redirect:/order/%d?msg=%s".formatted(order.getId(), Ut.url.encode("결제가 완료되었습니다."));
         } else {
--- 2Week_Mission/src/main/java/com/ll/exam/Week_Mission/app/order/service/OrderService.java
+++ 2Week_Mission/src/main/java/com/ll/exam/Week_Mission/app/order/service/OrderService.java
@@ -80,7 +80,7 @@ public class OrderService {
             throw new RuntimeException("예치금이 부족합니다.");
         }
 
-        memberService.addCash(buyer, payPrice * -1, "주문결제__예치금결제");
+        memberService.addCash(buyer, payPrice * -1, "주문__%d__사용__예치금".formatted(order.getId()));
 
         order.setPaymentDone();
         orderRepository.save(order);
@@ -89,7 +89,7 @@ public class OrderService {
     @Transactional
     public void refund(Order order) {
         int payPrice = order.getPayPrice();
-        memberService.addCash(order.getBuyer(), payPrice, "주문환불__예치금환불");
+        memberService.addCash(order.getBuyer(), payPrice, "주문__%d__환불__예치금".formatted(order.getId()));
 
         order.setRefundDone();
         orderRepository.save(order);
@@ -109,12 +109,17 @@ public class OrderService {
 
     // 토스페이먼츠를 예치금 시스템에 편입
     @Transactional
-    public void payByTossPayments(Order order) {
+    public void payByTossPayments(Order order, long useRestCash) {
         Member buyer = order.getBuyer();
         int payPrice = order.calculatePayPrice();
 
-        memberService.addCash(buyer, payPrice, "주문결제충전__토스페이먼츠");
-        memberService.addCash(buyer, payPrice * -1, "주문결제__토스페이먼츠");
+        long pgPayPrice = payPrice - useRestCash;
+        memberService.addCash(buyer, pgPayPrice, "주문__%d__충전__토스페이먼츠".formatted(order.getId()));
+        memberService.addCash(buyer, pgPayPrice * -1, "주문__%d__사용__토스페이먼츠".formatted(order.getId()));
+
+        if ( useRestCash > 0 ) {
+            memberService.addCash(buyer, useRestCash * -1, "주문__%d__사용__예치금".formatted(order.getId()));
+        }
 
         order.setPaymentDone();
         orderRepository.save(order);
